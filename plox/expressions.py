3189105dc9c582f9d752fc9efd244c717077b87f
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2020-12-25 09:21:29 -0800

    Yikes.

--- plox/expressions.py
+++ plox/expressions.py
@@ -1,10 +1,38 @@
-from plox.token import Token
+from plox import token
 
 class Expr:
-    pass
+     pass
 
 class Binary(Expr):
-    def __init__(self, left: Expr, operator: Token, right: Expr):
-        self.left = left;
-        self.operator = operator;
-        self.right = right;
+    def __init__(
+        self,
+        left: Expr,
+        operator: token.Token,
+        right: Expr
+    ) -> None:
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+class Grouping(Expr):
+    def __init__(
+        self,
+        expression: Expr
+    ) -> None:
+        self.expression = expression
+
+class Literal(Expr):
+    def __init__(
+        self,
+        value: token.Literal
+    ) -> None:
+        self.value = value
+
+class Unary(Expr):
+    def __init__(
+        self,
+        operator: token.Token,
+        right: Expr
+    ) -> None:
+        self.operator = operator
+        self.right = right
--- tools/generate_ast.py
+++ tools/generate_ast.py
@@ -1,7 +1,5 @@
 import sys
 
-from typing import List
-
 def main() -> None:
     # sys.argv[0] is the script name, which we drop.
     args = sys.argv[1:]
@@ -10,23 +8,58 @@ def main() -> None:
         print('Usage: generate_ast <output_directory>')
         sys.exit(64)
 
+    module_name = 'expressions'
+    base_name = 'Expr'
     output_directory = args[0]
+    output_path = f'{output_directory}/{module_name}.py'
+    indent = '    '
 
     types = [
-        'Binary   : Expr left, Token operator, Expr right',
+        'Binary   : Expr left, token.Token operator, Expr right',
         'Grouping : Expr expression',
-        'Literal  : Literal value',
-        'Unary    : Token operator, Expr right'
+        'Literal  : token.Literal value',
+        'Unary    : token.Token operator, Expr right'
     ]
 
-    define_ast(output_directory, 'Expr', types)
+    with open(output_path, 'w') as file:
+        file.write('from plox import token\n')
+        file.write('\n')
 
-def define_ast(
-    output_directory: str,
-    base_name: str,
-    types: List[str]
-) -> None:
-    pass
+        file.write(f'class {base_name}:\n')
+        file.write(f'{indent} pass\n')
+        file.write('\n')
+
+        for i in range(len(types)):
+            type = types[i]
+
+            class_name = type.split(':')[0].strip()
+            fields = type.split(':')[1].strip().split(', ')
+
+            file.write(f'class {class_name}({base_name}):\n')
+
+            file.write(f'{indent}def __init__(\n')
+            file.write(f'{indent}{indent}self,\n')
+
+            for j in range(len(fields)):
+                field = fields[j]
+
+                name = field.split(' ')[1]
+                annotation = field.split(' ')[0]
+
+                file.write(f'{indent}{indent}{name}: {annotation}')
+
+                if j < len(fields) - 1: file.write(',')
+
+                file.write('\n')
+
+            file.write(f'{indent}) -> None:\n')
+
+            for field in fields:
+                name = field.split(' ')[1]
+
+                file.write(f'{indent}{indent}self.{name} = {name}\n')
+
+            if i < len(types) - 1: file.write('\n')
 
 if __name__ == '__main__':
     main()
