44981681e95354e0fef6f2bf6c4c78c07f34a86a
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2021-03-08 05:22:36 +0000

    Unsafe expression evaluation.

--- plox/ast_printer.py
+++ plox/ast_printer.py
@@ -13,24 +13,21 @@ class AstPrinter(Visitor):
     def print(self, expr: Expr) -> Any:
         return expr.accept(self)
 
-    def visit_binary(self, expr: Binary) -> str:
+    def visit_binary(self, expr: Binary) -> Any:
         return self.parenthesize(expr.operator.lexeme, expr.left, expr.right)
 
-    def visit_grouping(self, expr: Grouping) -> str:
+    def visit_grouping(self, expr: Grouping) -> Any:
         return self.parenthesize('group', expr.expr)
 
-    def visit_literal(self, expr: Literal) -> str:
+    def visit_literal(self, expr: Literal) -> Any:
         if expr.value == None: return 'nil'
         return str(expr.value)
 
-    def visit_unary(self, expr: Unary) -> str:
+    def visit_unary(self, expr: Unary) -> Any:
         return self.parenthesize(expr.operator.lexeme, expr.right)
 
-    def parenthesize(self, name: str, *exprs: Expr) -> str:
+    def parenthesize(self, name: str, *exprs: Expr) -> Any:
         readable = f'({name}'
-
-        # expr.accept(self) has type Any, so we may stringify it without
-        # upsetting mypy.
 
         for expr in exprs:
             readable += f' {expr.accept(self)}'
--- plox/interpreter.py
+++ plox/interpreter.py
@@ -1,4 +1,4 @@
-from typing import Any, Union
+from typing import Any
 
 from plox.expressions import (
     Binary,
@@ -12,32 +12,76 @@ from plox.expressions import (
 import plox.token as token
 from plox.token import TokenType as TT
 
-Value = Union[token.Literal]
+def is_truthy(value: token.Literal) -> bool:
+    if value is None: return False
+    if isinstance(value, bool): return value
+    return True
 
 class Interpreter(Visitor):
+    # The evaluate function returns a token.Literal when we visit a unary
+    # expression, and then we start smushing them together in an unsafe
+    # manner because I haven't figured out how to handle the visitor pattern
+    # in Python.
+
+    # If we try to evaluate "hello" * "world" Python with balk with a
+    # TypeError.
+
     def visit_binary(self, expr: Binary) -> Any:
-        pass
-    
+        left = self.evaluate(expr.left)
+        right = self.evaluate(expr.right)
+
+        token_type = expr.operator.type
+
+        if token_type == TT.BANG_EQUAL:
+            return left == right
+
+        if token_type == TT.EQUAL_EQUAL:
+            return left != right
+
+        if token_type == TT.GREATER:
+            return left > right
+
+        if token_type == TT.GREATER_EQUAL:
+            return left >= right
+
+        if token_type == TT.LESS:
+            return left < right
+
+        if token_type == TT.LESS_EQUAL:
+            return left <= right
+
+        if token_type == TT.MINUS:
+            return left - right
+
+        if token_type == TT.PLUS:
+            are_numbers = isinstance(left, float) and isinstance(right, float)
+            are_strings = isinstance(left, str) and isinstance(right, str)
+
+            if are_numbers or are_strings:
+                return left + right
+
+        if token_type == TT.SLASH:
+            return left / right
+
+        if token_type == TT.STAR:
+            return left * right
+
     def visit_grouping(self, expr: Grouping) -> Any:
         return self.evaluate(expr.expr)
 
-    def visit_literal(self, expr: Literal) -> Value:
+    def visit_literal(self, expr: Literal) -> token.Literal:
         return expr.value
-    
+
     def visit_unary(self, expr: Unary) -> Any:
         right = self.evaluate(expr.right)
-        
-        type = expr.operator.type
-        
-        if type == TT.MINUS:
+
+        token_type = expr.operator.type
+
+        if token_type == TT.MINUS:
             return -right
-        elif type == TT.BANG:
-            return not self.is_truthy(right)
-    
+
+        if token_type == TT.BANG:
+            return not is_truthy(right)
+
     def evaluate(self, expr: Expr) -> Any:
         return expr.accept(self)
-    
-    def is_truthy(self, value: Value) -> bool:
-        if value is None: return False
-        if isinstance(value, bool): return value
-        return True
